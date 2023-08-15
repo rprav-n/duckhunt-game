4cb766696a2667a033fc933ea79966e6932fbcd2
Praveen <rpraveen.engineer@gmail.com>
2023-08-15 17:37:40 +0530

    Added dog animation

--- scripts/Dog.cs
+++ scripts/Dog.cs
@@ -1,18 +1,23 @@
 using Godot;
 using System;
 
-public class Dog : Area2D
+public class Dog : Node2D
 {
 
-	private Anima
+	private AnimatedSprite animatedSprite;
 
 	public override void _Ready()
 	{
-		
+		animatedSprite = GetNode<AnimatedSprite>("Area2D/AnimatedSprite");
 	}
 
 	public override void _Process(float delta)
 	{
 		
+	}
+	
+	public void ChangeAnimation() 
+	{
+		animatedSprite.Animation = animatedSprite.Animation == "walk" ? "sniff" : "walk";
 	}
 }
