d7ba323c7c2f3093ee5ca5ad98d96a5db352de36
Praveen <rpraveen.engineer@gmail.com>
2023-08-15 16:54:15 +0530

    Initial Commit

---
+++ scripts/Dog.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class Dog : Area2D
+{
+
+	private Anima
+
+	public override void _Ready()
+	{
+		
+	}
+
+	public override void _Process(float delta)
+	{
+		
+	}
+}
